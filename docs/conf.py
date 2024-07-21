adbc921cf04553ccb9827bebb5210d92c72ccbde
Wizard of z/OS <wizard@zdevops.com>
2024-07-21 22:50:10 +0200

    Enable search?

--- docs/conf.py
+++ docs/conf.py
@@ -29,7 +29,7 @@ import sphinx_rtd_theme
 
 html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
 html_theme = 'sphinx_rtd_theme'
-
+ihtml_static_path = ['_static']
 # -- Options for markdown output -------------------------------------------------
 
 # remove .md to ensure links work on github's Wiki
